4f647a5ca088ae93623c2a9d401391b3ac1d1178
franksm <franksmhuang@gmail.com>
2020-04-06 16:06:19 +0800

    composer

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
+class CreateSqlite3{
+    $pdo = new PDO('sqlite:syslog.sqlite3');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$pdo = new PDO('sqlite:syslog.sqlite3');
-$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-$pdo->exec("CREATE TABLE IF NOT EXISTS logs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    level VARCHAR(10) NOT NULL,
-    message TEXT NOT NULL
-)");
-
+    $pdo->exec("CREATE TABLE IF NOT EXISTS logs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                level VARCHAR(10) NOT NULL,
+                message TEXT NOT NULL
+    )");
+}
 
 
 
