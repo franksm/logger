d6584bdf47448fb31a486626291f8271202a69e9
franksm <franksmhuang@gmail.com>
2020-04-06 16:27:44 +0800

    123

--- CreateSqlite3.php
+++ CreateSqlite3.php
@@ -2,14 +2,17 @@
 namespace CreateSqlite3;
 
 class CreateSqlite3{
-    $pdo = new PDO('sqlite:syslog.sqlite3');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-    $pdo->exec("CREATE TABLE IF NOT EXISTS logs (
+    function desc(){
+        $pdo = new PDO('sqlite:syslog.sqlite3');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+        $pdo->exec("CREATE TABLE IF NOT EXISTS logs (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 level VARCHAR(10) NOT NULL,
                 message TEXT NOT NULL
-    )");
+        )");
+    }
 }
 
 
